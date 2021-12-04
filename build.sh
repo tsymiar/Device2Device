8f4790a4137c0d2530157f020338360c97dc1dcc
“tsymiar” <909006258@qq.com>
2021-12-04 11:52:34 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,41 @@
+toolPath=~/build-tools
+yum install java-1.8.0-openjdk-devel.x86_64
+if [ ! -d "${toolPath}" ]; then mkdir ${toolPath}; fi;
+cd ${toolPath}
+if [ ! -d "${toolPath}/android-studio/bin" ]; then
+    wget https://r1---sn-ni5een7z.gvt1.com/edgedl/android/studio/ide-zips/2020.3.1.25/android-studio-2020.3.1.25-linux.tar.gz
+    tar zvxf android-studio-2020.3.1.25-linux.tar.gz
+    rm -f android-studio-2020.3.1.25-linux.tar.gz
+else
+    echo "found: `ls | grep android-studio | head -1`"
+fi
+export PATH=${PATH}:${toolPath}/android-studio/bin
+if [ ! -d "${toolPath}/cmdline-tools/bin" ]; then
+    wget https://dl.google.com/android/repository/commandlinetools-linux-7583922_latest.zip
+    unzip commandlinetools-linux-7583922_latest.zip
+    rm -f commandlinetools-linux-7583922_latest.zip
+else
+    echo "found: `ls | grep cmdline-tools | head -1`"
+fi
+export PATH=${PATH}:${toolPath}/cmdline-tools/bin
+if [ ! -d "${toolPath}/sdk_root/platform-tools" ]; then
+    sdkmanager "ndk-bundle" --channel=3 --channel=1 "cmake;3.18.1" 'ndk;23.0.7599858' "platform-tools" "build-tools;30.0.3" "platforms;android-31" "cmake;3.18.1" --sdk_root=`pwd`/sdk_root
+    export ANDROID_NDK_HOME=`pwd`/sdk_root/ndk-bundle
+else
+    echo "found: `ls | grep sdk_root | head -1`"
+fi
+export PATH=${PATH}:${toolPath}/sdk_root/platform-tools
+export ANDROID_SDK_ROOT=${toolPath}/sdk_root
+if [ ! -d "${toolPath}/gradle-7.0.2/bin" ]; then
+    wget https://downloads.gradle-dn.com/distributions/gradle-7.0.2-bin.zip
+    unzip gradle-7.0.2-bin.zip
+    rm -f gradle-7.0.2-bin.zip
+else
+    echo "found: `ls | grep gradle | head -1`"
+fi
+export PATH=${PATH}:${toolPath}/gradle-7.0.2/bin
+cd -
+cd app/src/main/cpp/scadop
+git submodule update --init --recursive
+cd -
+gradle clean assembleDebug --no-daemon --stacktrace
